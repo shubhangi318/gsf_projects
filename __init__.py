79ba2ff006bdd5c7de4ac2a6e2743f469cc6f315
Shubhangi Bhatia <71550499+shubhangi318@users.noreply.github.com>
2024-09-26 16:40:27 +0530

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,39 @@
+class InitInfo():
+    request_headers = {
+        'authority': 'public-api.goldstandard.org',
+        'accept': 'application/json, text/plain, */*',
+        'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8,hi;q=0.7',
+        'origin': 'https://registry.goldstandard.org',
+        'referer': 'https://registry.goldstandard.org/',
+        'sec-ch-ua': '"Not A(Brand";v="99", "Google Chrome";v="121", "Chromium";v="121"',
+        'sec-ch-ua-mobile': '?0',
+        'sec-ch-ua-platform': '"Linux"',
+        'sec-fetch-dest': 'empty',
+        'sec-fetch-mode': 'cors',
+        'sec-fetch-site': 'same-site',
+        'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36',
+    }
+
+    request_base_params = {
+        'query': '',
+        'size': '25',
+        'sortColumn': '',
+        'sortDirection': '',
+    }
+
+    url_headers = {
+        'Accept': '*/*',
+        'Accept-Language': 'en-GB,en-US;q=0.9,en;q=0.8,hi;q=0.7',
+        'Connection': 'keep-alive',
+        'Origin': 'https://platform.sustain-cert.com',
+        'Referer': 'https://platform.sustain-cert.com/',
+        'Sec-Fetch-Dest': 'empty',
+        'Sec-Fetch-Mode': 'cors',
+        'Sec-Fetch-Site': 'cross-site',
+        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36',
+        'request-id': '|881c844b10d840ce91acc0d5dcb5de72.1136db9413a94074',
+        'sec-ch-ua': '"Not A(Brand";v="99", "Google Chrome";v="121", "Chromium";v="121"',
+        'sec-ch-ua-mobile': '?0',
+        'sec-ch-ua-platform': '"Linux"',
+        'traceparent': '00-881c844b10d840ce91acc0d5dcb5de72-1136db9413a94074-01',
+    }
